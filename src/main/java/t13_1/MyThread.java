df0faf07bdaa3b6ef58d9d149a121b771368a04d
zhongdepeng <fe>
2016-12-23 17:02:35 +0800

    zhang

---
+++ src/main/java/t13_1/MyThread.java
@@ -0,0 +1,12 @@
+package t13_1;
+
+/**
+ * Created by zhongdepeng on 2016/12/23.
+ */
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        
+    }
+}
