514f97b02bde2a2b9f5936687b770327a3278ac2
zhongdepeng <fe>
2017-01-24 16:57:50 +0800

    init

--- src/main/java/Test/Task.java
+++ src/main/java/Test/Task.java
@@ -1,5 +1,7 @@
 package Test;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 /**
  * Created by zhongdepeng on 2017/1/24.
  */
@@ -12,6 +14,8 @@ public class Task {
             Thread.sleep(3000);
             getData1="长时间处理任务后从远程返回的值1 threadName="+Thread.currentThread().getName();
             getData2="长时间处理任务从远程返回的值2 threadNam="+Thread.currentThread().getName();
+            System.out.println(getData1);
+            System.out.println(getData2);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
---
+++ src/main/java/synchronizedOneThreadIn/ObjectService.java
@@ -0,0 +1,9 @@
+package synchronizedOneThreadIn;
+
+/**
+ * Created by zhongdepeng on 2017/1/24.
+ */
+public class ObjectService {
+
+
+}
